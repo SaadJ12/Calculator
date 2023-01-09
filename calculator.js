d22a5794efabb7531df36011edd37f85f018eab6
Saad <saadjamshaid000@gmail.com>
2023-01-09 18:00:30 +0500

    Add functions for basic arithmetic operator

---
+++ calculator.js
@@ -0,0 +1,32 @@
+// Following is function for add
+const add = function(a, b) {
+    return a+b;
+}
+
+// Following is a function for subtraction
+const subtract = function(a, b) {
+    return a-b;
+}
+
+// Following is a function for multiplication
+const multiply = function(a, b) {
+    return a*b;
+}
+
+// Following is a function of divide
+const divide = function(a, b) {
+    return a/b;
+}
+
+// Operate Function
+const operate = function(a, operator, b) {
+    switch(operator) {
+    case "+":
+        return add(a, b); 
+    case "-":
+        return subtract(a, b);
+    case "*":
+        return multiply(a, b);
+    case "/":
+        return divide(a, b);
+    }   }
