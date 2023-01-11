d66974d33d328217b796fa9ba1542de8462bb523
Saad <saadjamshaid000@gmail.com>
2023-01-12 02:29:21 +0500

    Add functionality for addition operation

--- calculator.js
+++ calculator.js
@@ -34,22 +34,45 @@ const operate = function(a, operator, b) {
 let arithmeticCalc = {
     a: 0,
     Operand: 0,
-    secondNumber: 0
+    b: 0
 };    
 
+let step = 1;
 const buttons = document.querySelectorAll('.digit');
 buttons.forEach(element => {
     element.addEventListener('click', (e) => {
     const display = document.querySelector('.display');
-    display.innerText += e.target.innerText; 
-        arithmeticCalc.a = display.innerText;
-        console.log(arithmeticCalc.a);
-})
-})
+    display.innerText = e.target.innerText; 
+    if(step === 1) {
+       arithmeticCalc.a = parseFloat(display.innerText);
+       step = 2;
+    } 
+    })
+});
+
 const operations = document.querySelectorAll('.operand');
 operations.forEach(newelement => {
     newelement.addEventListener('click', (e) => {
     const newdisplay = document.querySelector('.display');
     newdisplay.innerText += e.target.innerText;
+    arithmeticCalc.Operand = newdisplay.innerText;
+    step = 3;
     })
-})
+});
+
+const secondNumber = document.querySelectorAll('.digit');
+secondNumber.forEach(element => {
+    element.addEventListener('click', (e) => {
+    const display2 = document.querySelector('.display');
+    display2.innerText = e.target.innerText;
+    arithmeticCalc.b = parseFloat(display2.innerText);
+    // perform the calculation using operate function
+const sol = document.querySelector('.solution');
+    sol.addEventListener('click', (e) => {
+    const result = add(arithmeticCalc.a, arithmeticCalc.b);
+    console.log(result);
+    step = 1;
+    });
+    })
+
+    }) 
