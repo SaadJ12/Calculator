30a9282ceedcd776168a4a2d9b1d59e50d2746a3
Saad <saadjamshaid000@gmail.com>
2023-01-13 02:15:47 +0500

    Revised the code to make it work for multiple numbers
    The code is thoroughly revised to make it work for multiple numbers previously it was not letting user to input more than 1 digit. This is done by making a separate variable "currentNumber"

--- calculator.js
+++ calculator.js
@@ -30,64 +30,62 @@ const operate = function(a, operator, b) {
     case "/":
         return divide(a, b);
     }   }
-
-let arithmeticCalc = {
-    a: 0,
-    Operand: 0,
-    b: 0
-};    
-
-let step = 1;
-const buttons = document.querySelectorAll('.digit');
-buttons.forEach(element => {
-    element.addEventListener('click', (e) => {
-    const display = document.querySelector('.display');
-    if(step === 1) {
-        arithmeticCalc.a = parseFloat(display.innerText + e.target.innerText);
-        display.innerText = arithmeticCalc.a;
-        step = 2;
-    } else if (step === 3){
-        arithmeticCalc.b = parseFloat(display.innerText + e.target.innerText);
-        display.innerText = arithmeticCalc.b;
-        step = 4;
-    } 
-    })
-});
-                                       
-const operations = document.querySelectorAll('.operand');
-operations.forEach(newelement => {
-    newelement.addEventListener('click', (e) => {
-    const newdisplay = document.querySelector('.display');
-    if(step === 2) {
-    newdisplay.innerText += e.target.innerText;
-    arithmeticCalc.Operand = e.target.innerText;
-    step = 3;
-    }
-    })
-});
-
-const sol = document.querySelector('.solution');
-sol.addEventListener('click', (e) => {
-    if(step === 4) {
-        let result;
-        switch(arithmeticCalc.Operand) {
-            case "+":
-                result = add(arithmeticCalc.a, arithmeticCalc.b);
-                break;
-            case "-":
-                result = subtract(arithmeticCalc.a, arithmeticCalc.b);
-                break;
-            case "*":
-                result = multiply(arithmeticCalc.a, arithmeticCalc.b);
-                break;
-            case "/":
-                result = divide(arithmeticCalc.a, arithmeticCalc.b);
-                break;
+    let currentNumber = 0;
+    let arithmeticCalc = {
+        a: 0,
+        Operand: 0,
+        b: 0
+    };
+    let step = 1;
+    
+    const buttons = document.querySelectorAll('.digit');
+    buttons.forEach(element => {
+        element.addEventListener('click', (e) => {
+            const display = document.querySelector('.display');
+            currentNumber = currentNumber * 10 + parseInt(e.target.innerText);
+            display.innerText = currentNumber;
+        });
+    });
+    
+    const operations = document.querySelectorAll('.operand');
+    operations.forEach(newelement => {
+        newelement.addEventListener('click', (e) => {
+            if (step === 1) {
+                arithmeticCalc.a = currentNumber;
+                currentNumber = 0;
+                arithmeticCalc.Operand = e.target.innerText;
+                step = 2;
+            }
+        });
+    });
+    
+    const sol = document.querySelector('.solution');
+    sol.addEventListener('click', (e) => {
+        if (step === 2) {
+            arithmeticCalc.b = currentNumber;
+            currentNumber = 0;
+            step = 3;
+    
+            let result;
+            switch(arithmeticCalc.Operand) {
+                case "+":
+                    result = add(arithmeticCalc.a, arithmeticCalc.b);
+                    break;
+                case "-":
+                    result = subtract(arithmeticCalc.a, arithmeticCalc.b);
+                    break;
+                case "*":
+                    result = multiply(arithmeticCalc.a, arithmeticCalc.b);
+                    break;
+                case "/":
+                    result = divide(arithmeticCalc.a, arithmeticCalc.b);
+                    break;
+            }
+            console.log(result);
+            const display = document.querySelector('.display');
+            display.innerText = result;
+            step = 1;
         }
-        console.log(result);
-        const display = document.querySelector('.display');
-        display.innerText = result;
-        step = 1;
-    }
-});
+    });
+    
 
